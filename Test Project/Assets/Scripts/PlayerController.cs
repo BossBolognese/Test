9b81b74d6d2e071877ddf6d06d101f99a7f1226d
Boss <maximpetrov889@gmail.com>
2017-09-05 12:06:23 +0200

    Revert "Revert "Adds working Inventory system""
    This reverts commit f5cbdcf962554b68a1c4c24034d2f29a5ca2d5fb.

--- Test Project/Assets/Scripts/PlayerController.cs
+++ Test Project/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -23,6 +24,12 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+
         if (Input.GetMouseButtonDown(0))
         {
             //shoot ray from mouse pos
